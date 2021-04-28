d47eca4c95c826f1fea8d6b75729b976682dc522
egocentryczny <mgal7337@outlook.com>
2021-04-28 23:58:36 +0200

    Further improvements

--- week1/percolation/Percolation.java
+++ week1/percolation/Percolation.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private final WeightedQuickUnionUF uF;
-    private final boolean[][] grid;
+    private final boolean[] grid;
     private final int virtualTopSite;
     private final int virtualBottomSite;
     private final int gridSize;
@@ -18,43 +18,38 @@ public class Percolation {
         this.openSites = 0;
         this.gridSize = n;
 
-        this.grid = new boolean[n][n];
-        // for (int i = 1; i <= n; i++) {
-        //     for (int j = 1; j <= n; j++) {
-        //         this.grid[i][j] = new Site(i, j);
-        //     }
-        // }
+        this.grid = new boolean[n*n+2];
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        this.grid[row][col] = true;;
+        this.grid[row * col] = true;
         this.openSites += 1;
 
-        if(row == 1) {
+        if (row == 1) {
             uF.union(this.virtualTopSite, col * row);
-        } else if(row == this.gridSize) {
+        } else if (row == this.gridSize) {
             uF.union(this.virtualBottomSite, col * row);
         } else {
-            if (isOpen(row + 1, col)) uF.union(row * col, (row + 1) * col);
-            if (isOpen(row - 1, col)) uF.union(row * col, (row - 1) * col);
+            if (isOpen(row + 1, col)) uF.union(getIndex(row, col), getIndex((row + 1), col);
+            if (isOpen(row - 1, col)) uF.union(getIndex(row, col), getIndex((row - 1),  col));
             if (col + 1 <= gridSize) {
-                if (isOpen(row, col + 1)) uF.union(row * col, row * (col + 1));
+                if (isOpen(row, col + 1)) uF.union(getIndex(row, col), getIndex(row, col + 1));
             }
             if (col - 1 > 0) {
-                if (isOpen(row, col - 1)) uF.union(row * col, row * (col - 1));
+                if (isOpen(row, col - 1)) uF.union(getIndex(row, col), getIndex(row, col-1));
             }
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return this.grid[row][col];
+        return this.grid[row * col];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return uF.find(0) == uF.find(row * col);
+        return uF.find(0) == uF.find(getIndex(row, col));
     }
 
     // returns the number of open sites
@@ -65,5 +60,9 @@ public class Percolation {
     // does the system percolate?
     public boolean percolates() {
         return uF.find(0) == uF.find(this.gridSize * this.gridSize + 1);
+    }
+
+    private int getIndex(int row, int col) {
+        return (row - 1) * gridSize + col;
     }
 }
